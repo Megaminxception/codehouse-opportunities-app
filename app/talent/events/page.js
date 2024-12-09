9917fa580e6b5149949717bac9878d4971ab08cb
Zachary Dorcinville <zdorcinville@gmail.com>
2024-12-09 09:44:12 -0500

    add talent events page directory

---
+++ app/talent/events/page.js
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+
+export default function Events() {
+  return (
+    <Flex bg="primaryWhite" minH="100vh" justify="center" align="center">
+      <p className="text-black">events page</p>
+    </Flex>
+  );
+}
