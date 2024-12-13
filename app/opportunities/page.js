62f9eb4bfe130e2da3de6bda8ccef4f8304330e8
MohamedSy1 <mohmsy49@gmail.com>
2024-12-13 15:00:35 -0500

    added opportunityfolder inside components directory || added opportunities page

---
+++ app/opportunities/page.js
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react"
+
+export default function Opportunities() {
+   return (
+    <Flex bg="primaryWhite" minH="100vh" justify="center" align="center" className="text-black">
+        <h1>Hello</h1>
+    </Flex>
+   )
+}
---
+++ components/opportunities/JobComponent/JobContainer.jsx
@@ -0,0 +1,18 @@
+
+const OPPORTUNITIES_KEY = process.env.OPPORTUNITIES_KEY;
+const table_name = "Opportunities";
+
+const opportunityDescription = {
+    index: 1,
+    fieldName: "Partner",
+    DataType: 
+}
+
+const JobContainer = () => {
+
+    return (
+        
+    )
+}
+
+export default JobContainer;    
