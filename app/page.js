0f13c1aa4d7e4438626c7699fa5c5fec5377148a
Aaron Castillo <a.castillo3290@gmail.com>
2024-12-14 10:31:24 -0500

    made a fields list to create fieldset easier

--- app/page.js
+++ app/page.js
@@ -2,12 +2,5 @@ import { Flex } from "@chakra-ui/react";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <Flex
-      bg='primaryWhite'
-      minH='100vh'
-    >
-      
-    </Flex>
-  );
+  return <Flex bg="primaryWhite" minH="100vh"></Flex>;
 }
--- app/partners/submit-opportunity/page.js
+++ app/partners/submit-opportunity/page.js
@@ -1,19 +1,64 @@
-import { Fieldset, Stack, Input } from "@chakra-ui/react";
+import { Fieldset, Stack, Center, Textarea, Input } from "@chakra-ui/react";
 import { Field } from "@/components/ui/field";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
 import {
   NativeSelectField,
   NativeSelectRoot,
 } from "@/components/ui/native-select";
+import { text } from "express";
+
+const fields = [
+  // this would be replaced with actual data from airtable
+  {
+    label: "Partner*",
+    text: "Not seeing your organization? Register here.",
+    type: "select",
+    name: "Partner",
+    items: ["ABC company", "123 company", "Other"],
+  },
+  {
+    label: "Opportunity Type*",
+    type: "select",
+    name: "Opportunity Type",
+    items: ["ABC company", "123 company", "Other"],
+  },
+  {
+    label: "Time and Date of Event",
+    type: "select",
+    name: "Time and Date of Event*",
+    items: ["ABC company", "123 company", "Other"],
+  },
+  {
+    label: "Title of Opportunity*",
+    type: "select",
+    name: "Title of Opportunity",
+    items: ["ABC company", "123 company", "Other"],
+  },
+  {
+    label: "Opportunity URL*",
+    type: "select",
+    name: "Opportunity URL",
+    items: ["ABC company", "123 company", "Other"],
+  },
+  {
+    label: "Opportunity Description*",
+    type: "textarea",
+    name: "Opportunity Description",
+    placeholder: "We're Hiring!",
+  },
+];
 
 export default function SubmitOpportunity() {
+  {
+    /*
+        Todo: 12/14
+        - fix outline form bug
+        - fix font color gray
+        */
+  }
   return (
-    <Fieldset.Root
-      pos="relative"
-      top={20} // make this inherit instead of hard value
-      py="2rem"
-      w="full"
-      align="center"
-    >
+    <Fieldset.Root pt="120px" pb="274px" align="center">
       <Stack>
         <Fieldset.Legend>
           <h1 className="text-3xl font-bold">Submit Opportunities</h1>
@@ -23,75 +68,45 @@ export default function SubmitOpportunity() {
         </h2>
       </Stack>
 
-      <Fieldset.Content>
-        <Field label="Parter">
-          <NativeSelectRoot>
-            <NativeSelectField
-              name="Partner"
-              items={["ABC company", "123 company", "Other"]}
-            />
-          </NativeSelectRoot>
-        </Field>
+      <Center>
+        <Fieldset.Content w="1/2" align="center" px="4rem">
+          {fields.map((field, index) => (
+            <Field label={field.label} key={index}>
+              {field.text ? <p className="font-light">{field.text}</p> : null}
+              {field.type === "select" ? (
+                <NativeSelectRoot variant="outline">
+                  <NativeSelectField
+                    name={field.name}
+                    items={field.items}
+                    variant="outline"
+                    // color="primaryGray"
+                  />
+                </NativeSelectRoot>
+              ) : field.type === "textarea" ? (
+                <Textarea
+                  name={field.name}
+                  placeholder={field.placeholder}
+                  variant="outline"
+                />
+              ) : null}
+            </Field>
+          ))}
 
-        <Field label="Opportunity Type*">
-          <NativeSelectRoot>
-            <NativeSelectField
-              name="Opportunity Type"
-              items={["ABC company", "123 company", "Other"]}
-            />
-          </NativeSelectRoot>
-        </Field>
+          <Stack direction="row">
+            <Field label="Start Date">
+              <Input name="Start Date" placeholder="12/1/24" />
+            </Field>
+            <Field label="End Date">
+              <Input name="End Date" placeholder="12/31/24" />
+            </Field>
+          </Stack>
 
-        <Field label="Time and Date of Event">
-          <NativeSelectRoot>
-            <NativeSelectField
-              name="Time and Date of Event*"
-              items={["ABC company", "123 company", "Other"]}
-            />
-          </NativeSelectRoot>
-        </Field>
-
-        <Field label="Title of Opportunity">
-          <NativeSelectRoot>
-            <NativeSelectField
-              name="Title of Opportunity"
-              items={["ABC company", "123 company", "Other"]}
-            />
-          </NativeSelectRoot>
-        </Field>
-
-        <Field label="Title of Opportunity">
-          <NativeSelectRoot>
-            <NativeSelectField
-              name="Title of Opportunity"
-              items={["ABC company", "123 company", "Other"]}
-            />
-          </NativeSelectRoot>
-        </Field>
-
-        <Field label="Opportunity URL">
-          <NativeSelectRoot>
-            <NativeSelectField
-              name="Opportunity URL"
-              items={["ABC company", "123 company", "Other"]}
-            />
-          </NativeSelectRoot>
-        </Field>
-
-        <Field label="Opportunity Description">
-          <Input name="Opportunity Description" placeholder="We're Hiring!" />
-        </Field>
-
-        {/*
-        Todo 12/13
-        - add start/end date inputs
-        - verify checkbox
-        - submit button
-        - fix css for field width to fit in the page
-            - maybe use a stack comp.
-        - fix nav and footer positions
-        */}
-      </Fieldset.Content>
+          <Checkbox>I verify the responses above are correct.*</Checkbox>
+          <Button bg="primaryBlack" color="primaryWhite">
+            Submit
+          </Button>
+        </Fieldset.Content>
+      </Center>
     </Fieldset.Root>
   );
 }
