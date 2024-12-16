0802be549a59896918258f91ff41c0c0ed1a0df8
mayayr115 <mayayr115@gmail.com>
2024-12-16 12:33:55 -0500

    Made Branch, Reverted Changes, and Imported Calendar in correct page

--- app/talent/events/page.js
+++ app/talent/events/page.js
@@ -1,9 +1,12 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Center, Flex } from "@chakra-ui/react";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 export default function Events() {
   return (
     <Flex bg="primaryWhite" minH="100vh" justify="center" align="center">
       <p className="text-black">events page</p>
+      <Calendar></Calendar>
     </Flex>
   );
-}
+};
