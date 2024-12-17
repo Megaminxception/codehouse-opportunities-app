40bdb35f3d0608b14c527847ebd2b9e86f219983
Mohamed <mohamed.sy@compscihigh.org>
2024-12-17 13:30:52 -0500

    minor changes to styling

--- components/opportunities/JobComponent/opportunityModel.jsx
+++ components/opportunities/JobComponent/opportunityModel.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@chakra-ui/react";
 
 const OpportunityModel = () => {
-    return <Button bg={"#2C2C2C"} className="text-white p-2 "
+    return <Button bg={"#2C2C2C"} px={2} className="text-white text-sm rounded-lg py-0"
 
     >Learn More</Button>
 }
