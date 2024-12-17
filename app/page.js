3cb7719e648e7ed7de50e9e3f7e02695a0f5bbc2
alexaleoto <alexaricard@hotmail.com>
2024-12-17 11:21:08 -0500

    Build events filter inputs

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import { Box, Center, Flex } from "@chakra-ui/react";
 import Image from "next/image";
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+//import Calendar from 'react-calendar';
+//import 'react-calendar/dist/Calendar.css';
 
 export default function Home() {
   return (
@@ -9,9 +9,9 @@ export default function Home() {
       bg='primaryWhite'
       minH='100vh'
       >
-        <Center w='100%'>
+        {/* <Center w='100%'>
           <Calendar></Calendar>
-        </Center>
+        </Center> */}
     </Flex>
   );
 }
