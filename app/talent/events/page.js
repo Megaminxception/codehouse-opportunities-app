5d7cbeb32fa924ce9d3557020fa47ebb279e2e92
mayayr115 <mayayr115@gmail.com>
2024-12-18 15:00:33 -0500

    Added Calendar Functionality and CSS Visibility

--- app/talent/events/page.js
+++ app/talent/events/page.js
@@ -1,12 +1,37 @@
-import { Box, Center, Flex } from "@chakra-ui/react";
+"use client";
+import { Box, Center, Container, Flex } from "@chakra-ui/react";
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import { useState } from 'react'
+// import 'react-calendar/dist/Calendar.css';
 
 export default function Events() {
-  return (
-    <Flex bg="primaryWhite" minH="100vh" justify="center" align="center">
-      <p className="text-black">events page</p>
-      <Calendar></Calendar>
-    </Flex>
+
+  // State to store the selected date
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  // Function to update the state when a date is selected
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    console.log('Selected date:', date);
+  };
+  
+  return ( 
+    <>
+      <Flex bg="primaryWhite" minH="100vh" justify="center" align="center">
+        {/* <p className="text-black">events page</p> */}
+        <Container>
+          <div>
+          {/* Calendar component with onChange handler */}
+          <Calendar 
+            onChange={handleDateChange} 
+            value={selectedDate} 
+          />
+          {/* Display the selected date */}
+          <p>Selected Date: {selectedDate.toDateString()}</p>
+        </div>
+          </Container>
+      </Flex>
+
+    </>
   );
 };
