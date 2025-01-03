75cb9aef18023a565c422ae05b9813707fc138d2
alexaleoto <alexaricard@hotmail.com>
2025-01-03 09:43:48 -0500

    moved EventItem to components

--- app/talent/events/page.js
+++ app/talent/events/page.js
@@ -1,7 +1,8 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Flex, Text, Input, Box, Button } from "@chakra-ui/react";
+import { Flex, Text, Input, Box } from "@chakra-ui/react";
+import EventItem from "@/components/EventItem";
 
 export default function Events() {
   const [searchValue, setSearchValue] = useState("");
@@ -188,102 +189,10 @@ export default function Events() {
         </Flex>
       </Box>
 
-      {events.map((event) => {
-  const startDate = event.EventDate ? new Date(event.EventDate) : null;
-
-  const formattedDate = startDate
-    ? `${startDate.toLocaleString("default", { month: "short" })} ${startDate.getDate()}`
-    : "N/A";
-
-  return (
-    <Box
-      key={event.id}
-      bg="white"
-      borderRadius="sm"
-      boxShadow="sm"
-      p={4} 
-      mb={4} 
-      width="90%"
-      maxW="700px"
-      mx="auto"
-      display="flex"
-      alignItems="center"
-      minH="100px"
-    >
-     {/* Date Circle */}
-<Box
-  bg="gray.800"
-  color="white"
-  textAlign="center"
-  borderRadius="full"
-  w="60px"
-  h="60px"
-  display="flex"
-  flexDirection="column"
-  justifyContent="center"
-  alignItems="center"
-  mr={5}
->
-  {startDate ? (
-    <>
-      <Text fontSize="xl" fontWeight="bold" lineHeight="1">
-        {startDate.getDate()}
-      </Text>
-      <Text fontSize="sm" fontWeight="bold" textTransform="uppercase" mt={0.5}>
-        {startDate.toLocaleString("default", { month: "short" })}
-      </Text>
-    </>
-  ) : (
-    <>
-      <Text fontSize="xl" fontWeight="bold">TBD</Text>
-    </>
-  )}
-</Box>
-
-
-   {/* Event Details */}
-<Flex flex="1" align="flex-start" direction="column">
-  {/* Event Title */}
-  <Text fontWeight="bold" fontSize="lg" mb={0.5} color="black" lineHeight="1.2">
-    {event.EventName}
-  </Text>
-
-  {/* Event Date */}
-  <Text fontSize="sm" color="black" mb={0.5} lineHeight="1.2">
-    {formattedDate}
-  </Text>
-
-  {/* Event Host */}
-  <Text fontSize="sm" color="black" mb={0.5} lineHeight="1.2">
-   {event.EventHost}
-  </Text>
-</Flex>
-
-      {/* Event Description */}
-      <Box maxW="45%" pl={4} pr={4}>
-        <Text fontSize="sm" color="gray.500" lineHeight="1.4">
-          {event.EventDescription}
-        </Text>
-      </Box>
-
-      {/* Attend Button */}
-      <Button
-        bg="gray.800"
-        color="white"
-        px={4}
-        py={2}
-        fontSize="sm"
-        ml={4}
-        borderRadius="sm"
-        _hover={{ bg: "gray.700" }}
-      >
-        Attend
-      </Button>
-    </Box>
-  );
-})}
-
-
+      {/* Event Items */}
+      {events.map((event) => (
+        <EventItem key={event.id} event={event} />
+      ))}
     </Flex>
   );
 }
