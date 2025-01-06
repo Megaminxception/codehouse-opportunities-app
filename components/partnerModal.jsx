16ce8f41a2f99ccc20be41c005c5bf0257655bfd
Erica Ponce <ericaponce2002@gmail.com>
2025-01-05 21:28:54 -0500

    created partner modal jsx file and imported from chakra ui

---
+++ components/partnerModal.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import {
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+
+
+export default function PartnerModal() {
+
+}
