1707020ef007558cea7f0a32f5c4b7544639588c
Erica Ponce <ericaponce2002@gmail.com>
2025-01-07 16:00:47 -0500

    created the view profile button as a modal. still need to center the popup and change the text.

--- app/talent/partners/page.js
+++ app/talent/partners/page.js
@@ -1,3 +1,29 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import PartnerModal from "@/components/partnerModal";
+
+import {
+  
+  Stack,
+  Center,
+  Textarea,
+  Input,
+  Heading,
+  Text,
+  Box,
+} from "@chakra-ui/react";
+
+export default function Partners() {
+  return (
+    <Stack textAlign="center" align="center" pt="120px" pb="274px">
+          
+            <Box textAlign="center">
+              {/* <Heading> */}
+                
+            <PartnerModal/>
+              {/* </Heading> */}
+            </Box>
+          </Stack>
+  )
+}
--- components/partnerModal.jsx
+++ components/partnerModal.jsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { For, HStack } from "@chakra-ui/react"
+import { Button } from "@/components/ui/button"
 import {
+  DialogActionTrigger,
   DialogBody,
   DialogCloseTrigger,
   DialogContent,
@@ -11,8 +14,41 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 
-
+const Demo = () => {
+  return (
+    <HStack wrap="wrap" gap="4" bgColor="red.200">
+          <DialogRoot
+            key={"center"}
+            placement="center"
+            motionPreset="slide-in-bottom"
+          >
+            <DialogTrigger asChild>
+              <Button variant="outline">View Profile</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Dialog Title</DialogTitle>
+              </DialogHeader>
+              <DialogBody>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </DialogBody>
+              <DialogFooter>
+                <DialogActionTrigger asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogActionTrigger>
+                <Button>Save</Button>
+              </DialogFooter>
+              <DialogCloseTrigger />
+            </DialogContent>
+          </DialogRoot>
+    </HStack>
+  )
+}
 
 export default function PartnerModal() {
 
+  return <Demo/>
 }
