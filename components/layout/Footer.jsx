d9a52cccd95d534236165c68bc922f3446a73d5e
crism05 <max42113@gmail.com>
2025-01-07 04:29:56 +0100

    Updated Footer.jsx to not be absolute

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="w-full absolute h-[250px] flex justify-start items-start border-t gap-24 bottom-0 text-black pl-24 pt-[2rem]">
+    <div className="w-full static h-[250px] flex justify-start items-start border-t gap-24 bottom-0 text-black pl-24 pt-[2rem]">
       <VStack gap="1.5rem" align='start'>
         <Heading className="font-bold">CodeHouse</Heading>
         <VStack gap="1rem" align='start'>
