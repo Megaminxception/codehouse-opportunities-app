1cbbd65eff2854a9c0d9bd76947122c5d810f0de
Erica Ponce <ericaponce2002@gmail.com>
2025-01-15 15:01:42 -0500

    added icon with the first letter of the company title and formatted the title

--- components/partnerModal.jsx
+++ components/partnerModal.jsx
@@ -4,16 +4,30 @@ import { Button } from "@/components/ui/button"
 import Link from 'next/link';
 import { Flex } from "@chakra-ui/react";
 
-const sectionStyle = { 
+const sectionStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(2, auto)",
-  // gridTemplateRows: "repeat(6, 1fr)",
-  gridColumnGap: "10px",
-  gridRowGap: "10px", };
+  // gridColumnGap: "10px",
+  gridRowGap: "10px",
+  alignItems: "center"
+};
 
-const h2Style = { fontSize: "40px", fontWeight: "bold" };
-const h3Style = { fontSize: "20px", fontWeight: "bold", paddingLeft: "100px" };
-const pStyle = { fontSize: "14px", color: "#555", paddingRight: "100px" };
+const h2Style = { fontSize: "40px", fontWeight: "bold", paddingLeft: "25px", };
+const h3Style = { fontSize: "20px", fontWeight: "bold", paddingLeft: "100px", width: "250px" };
+const pStyle = { fontSize: "14px", color: "#555", paddingRight: "100px", width: "350px" };
+const headerStyle = { display: "flex", alignItems: "center", paddingBottom: "5px", paddingTop: "5px" }
+const asideStyle = {
+  border: "1px solid black",
+  width: "50px",
+  height: "50px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  borderRadius: "50%",
+  color: "white",
+  background: "#2C2C2C",
+  marginLeft: "25px"
+}
 
 export const PartnerModal = ({ open, onCloseModal, title, partnerTypes, tier, involvedInPrograms, location, about, website }) => {
   return (
@@ -21,10 +35,6 @@ export const PartnerModal = ({ open, onCloseModal, title, partnerTypes, tier, in
       <Modal open={open} onClose={onCloseModal} center
         styles={{
           modal: {
-            // display: "grid",
-
-            // marginBottom: "16px",
-            // alignItems: "start",
             background: "#FFFFFF",
             borderRadius: "8px",
             boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
@@ -32,7 +42,13 @@ export const PartnerModal = ({ open, onCloseModal, title, partnerTypes, tier, in
           },
         }}
       >
-        <h2 style={h2Style}>{title}</h2>
+        <header style={headerStyle}>
+          <aside style={asideStyle}>
+            <h4>{title[0]}</h4>
+          </aside>
+          <h2 style={h2Style}>{title}</h2>
+
+        </header>
         <section style={sectionStyle}>
 
           <h3 style={h3Style}>Partner Type:</h3>
@@ -51,9 +67,14 @@ export const PartnerModal = ({ open, onCloseModal, title, partnerTypes, tier, in
           <p style={pStyle}>{about}</p>
 
           <h3 style={h3Style}>Website:</h3>
-          <p style={pStyle}>{website}</p>
+          <p style={{
+            fontSize: "14px", color: "#6F0010",paddingRight: "100px", 
+          }
+
+
+          }>{website}</p>
         </section>
-        <Link href="/talent/opportunities" style={{display:"flex", justifyContent:"center"}}>
+        <Link href="/talent/opportunities" style={{ display: "flex", justifyContent: "center" }}>
           <Button bg="#2C2C2C" color="white" >
             View Opportunites
           </Button>
